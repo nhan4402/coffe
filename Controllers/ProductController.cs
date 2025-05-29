cc8beb60b7254b958ebdafceca2b962a986f0f53
NguyenMinh <theminh1802@gmail.com>
2025-05-29 23:28:23 +0700

    Initial commit

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -28,11 +28,15 @@ namespace Shopping_Coffee.Controllers
         }
         public IActionResult Details(int? Id)
         {
-            if (Id == null) return RedirectToAction("Index");
+           if (Id == null) return RedirectToAction("Index");
 
-            var productsById = _dataContext.Products.FirstOrDefault(p => p.Id == Id);
-            return View(productsById);
+           var productsById = _dataContext.Products.FirstOrDefault(p => p.Id == Id);
+
+          return View(productsById);
         }
+
+       
+
 
     }
 }
