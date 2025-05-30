7724af976e8c70cd8838725dbb2d02bb6e08a769
NguyenMinh <theminh1802@gmail.com>
2025-05-30 18:47:54 +0700

    fear(Product):pagination

--- obj/Debug/net8.0/Shopping_Coffee.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopping_Coffee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6204366f6a7068288153e4be988c6cb2c6b41f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa353a370706e798831c40bc3fff8d5afce11a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
