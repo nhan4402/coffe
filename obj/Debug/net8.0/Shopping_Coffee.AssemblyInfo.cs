af1aafa54532056b2bc8b09fb11eb401912eb129
NguyenMinh <theminh1802@gmail.com>
2025-05-31 13:56:21 +0700

    feat(pagination): Add feat pagination in brand controller

--- obj/Debug/net8.0/Shopping_Coffee.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopping_Coffee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6204366f6a7068288153e4be988c6cb2c6b41f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5adeec877842d94589c9b1eaebb1977cb20bac73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
