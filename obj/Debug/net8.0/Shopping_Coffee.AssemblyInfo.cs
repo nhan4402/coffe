adc45cd55e7d0c4f2f041b60f38f7651c3710dbc
nhan4402 <phanthanhnhan808@gmail.com>
2025-06-18 20:21:44 +0700

    Push code lên nhánh vn

--- obj/Debug/net8.0/Shopping_Coffee.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopping_Coffee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58e1df9af6e2ce05a398ed398fad39a7a0e39ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
